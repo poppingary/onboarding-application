bb7fc3df5f0273772fc752373f92741e10505199
Gary <poppingary@gmail.com>
2024-03-12 16:48:38 -0400

    Initial setup of Spring Boot project with database configuration.

---
+++ onboarding/src/test/java/com/globaldatatek/onboarding/OnboardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globaldatatek.onboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnboardingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
